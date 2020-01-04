e4c50a5e51215bc97108783051d05e7a2f020730
ardeshir sepahsalar <ardeshir.org@gmail.com>
2020-01-04 16:55:36 -0600

    adding tag

--- code/terraform/01-why-terraform/web-server/main.tf
+++ code/terraform/01-why-terraform/web-server/main.tf
@@ -3,7 +3,7 @@ terraform {
 }
 
 provider "aws" {
-  region = "us-east-2"
+  region = "us-east-1"
 
   # Allow any 2.x version of the AWS provider
   version = "~> 2.0"
@@ -11,12 +11,15 @@ provider "aws" {
 
 resource "aws_instance" "app" {
   instance_type     = "t2.micro"
-  availability_zone = "us-east-2a"
+  availability_zone = "us-east-1a"
   ami               = "ami-0c55b159cbfafe1f0"
+
+  tags = {
+    Name = "terraform-ec2-t2micro"
+  }
 
   user_data = <<-EOF
               #!/bin/bash
               sudo service apache2 start
               EOF
 }
-
